f3bd0614921cc26cc4b5e33d145cc644bb34ec0a
akaryaka <a.karyaka.2dit@gmail.com>
2021-08-02 23:41:03 +0600

    feat(script.js): правки по массиву

--- js/script.js
+++ js/script.js
@@ -1,31 +1,33 @@
+
+let array = [
+	'Понедельник, вторник, среда, четверг, пятница, суббота, воскресенье',
+	'Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday'
+];
+
 console.log('Задача 1');
 console.log('...')
-
 
 console.log('Решаем через if');
 let lang = prompt("Какой язык у сайта?");
 if (lang == "ru") {
-	console.log('Понедельник, вторник, среда, четверг, пятница, суббота, воскресенье');
+	console.log( array[0] );
 } else if (lang == "en") {
-	console.log('Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday');
+	console.log( array[1] );
 }
 
 console.log('Решаем через switch-case');
 switch (lang) {
 	case "ru":
-		console.log('Понедельник, вторник, среда, четверг, пятница, суббота, воскресенье');
+		console.log( array[0] );
 		break;
 	case "en":
-		console.log('Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday');
+		console.log (array[1] );
 		break;
 }
 
 console.log('Решаем через многомерный массив без ифов и switch.');
 let choose = lang;
-let array = [
-	'Понедельник, вторник, среда, четверг, пятница, суббота, воскресенье',
-	'Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday'
-];
+
 array['ru'] = array[0] ;
 array['en'] = array[1] ;
 
